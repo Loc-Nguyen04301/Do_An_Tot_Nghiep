7512c3ebcef4468cd8766d8bf16e5ae67e2dcdf6
loc <nguyengialoc7@gmail.com>
2024-05-15 00:07:36 +0700

    fig bug redux-persist

--- front_end/src/redux-toolkit/rootReducer.ts
+++ front_end/src/redux-toolkit/rootReducer.ts
@@ -1,4 +1,4 @@
-import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import storage from 'redux-persist/lib/storage'
 import { persistReducer } from 'redux-persist'
 import authReducer from "./authSlice"
 import cartReducer, { CartState } from "./cartSlice"

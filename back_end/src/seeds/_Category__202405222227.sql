4d719c164868de8f000e8c8488d41dded7d31ab3
loc <nguyengialoc7@gmail.com>
2024-05-22 22:28:58 +0700

    backup data

---
+++ back_end/src/seeds/_Category__202405222227.sql
@@ -0,0 +1,8 @@
+INSERT INTO public."Category" (name,created_at,update_at) VALUES
+	 ('fatloss','2024-03-24 13:52:34.333',NULL),
+	 ('protein','2024-03-24 13:54:22.811',NULL),
+	 ('functional-food','2024-03-24 13:54:22.811',NULL),
+	 ('fashion','2024-03-24 13:54:22.86',NULL),
+	 ('gym-equipment','2024-03-24 13:54:22.906',NULL),
+	 ('energy-healthy','2024-03-24 13:54:22.763',NULL),
+	 ('fitness-gear','2024-03-24 13:54:22.954',NULL);
--- front_end/src/assets/data/categoryList.ts
+++ front_end/src/assets/data/categoryList.ts
@@ -1,9 +1,8 @@
 const categoryList = [
   { path: "fatloss", title: "Giảm mỡ - Giảm cân" },
-  { path: "energy-heathy", title: "Năng lượng - Sức khỏe" },
+  { path: "energy-healthy", title: "Năng lượng - Sức khỏe" },
   { path: "protein", title: "Protein - Tăng cân" },
   { path: "functional-food", title: "Thực phẩm chức năng" },
-  { path: "replacement", title: "WHEY & MEAL REPLACEMENT" },
   { path: "fashion", title: "Thời trang GYM" },
   { path: "gym-equipment", title: "Thiết bị phòng GYM" },
   { path: "fitness-gear", title: "Phụ kiện GYM" },

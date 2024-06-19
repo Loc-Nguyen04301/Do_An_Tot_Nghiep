ba672551f4696018080cde79cf6a26a6ba38d2a0
loc <nguyengialoc7@gmail.com>
2024-06-19 23:04:43 +0700

    fix detail page

--- front_end/src/pages/DetailProduct/index.tsx
+++ front_end/src/pages/DetailProduct/index.tsx
@@ -18,6 +18,7 @@ import "swiper/scss"
 import "swiper/scss/navigation"
 import { Tag } from "antd"
 import { addProductToWishList, IProductWishList, removeProductToWishList } from "@/redux-toolkit/wishListSlice"
+import ProductCard from "@/components/ProductCard"
 
 interface Category {
   id: number;
@@ -218,9 +219,21 @@ const DetailProduct = () => {
                   </span>
                 </div>
               </div>
-              <div className="border-b-[1px] border-dotted pb-1 text-lg">
-                Add to wishlist
-              </div>
+              {
+                product.available !== 0 && wishList.findIndex((item) => item.id === product.id) === -1 ?
+                  <div
+                    className="border-b-[1px] border-dotted pb-1 text-lg cursor-pointer hover:text-main-orange-color"
+                    onClick={() => { handleAddProductToWishList({ ...product }) }}
+                  >
+                    Add to wishlist
+                  </div> :
+                  <div
+                    className="border-b-[1px] border-dotted pb-1 text-lg cursor-pointer hover:text-main-orange-color"
+                    onClick={() => { handleRemoveItemToWishList(product.id) }}
+                  >
+                    Remove to wishlist
+                  </div>
+              }
               <div className="border-b-[1px] border-dotted pb-1 text-main-grey-color">
                 Mã: {product.id}
               </div>
@@ -263,60 +276,7 @@ const DetailProduct = () => {
                   {
                     relatedProducts.map(product =>
                       <SwiperSlide key={product.id}>
-                        <div className={`px-[10px] containerProduct`}>
-                          <div className="relative">
-                            {
-                              product.available === 0 && <Tag color="red" className="absolute top-0 left-0">Hết hàng</Tag>
-                            }
-                            <Link
-                              to={`${RoutePath.DetailProduct}/${product.id}`}
-                              className="text-center block mx-auto"
-                            >
-                              <img
-                                src={product.image}
-                                alt={product.name}
-                              />
-                            </Link>
-                            <div className={`hidden absolute bottom-0 w-full bg-main-orange-color text-center py-1 duration-500 showView`}>
-                              <span className="text-white font-semibold uppercase text-sm">
-                                Quick View
-                              </span>
-                            </div>
-                            {
-                              product.available !== 0 && wishList.findIndex((item) => item.id === product.id) === -1 ?
-                                <div
-                                  className={`hidden absolute top-0 right-0 w-[32px] h-[32px] border-2 border-main-grey-color rounded-full text-center opacity-95 duration-500 showView hover:bg-[#b20000] hover:border-[#b20000] cursor-pointer`}
-                                  title='Add to wishlist'
-                                  onClick={() => { handleAddProductToWishList({ ...product }) }}
-                                >
-                                  <HeartOutlined className='text-xl text-main-grey-color mt-[5px] show-heart' />
-                                </div>
-                                : product.available !== 0 && wishList.findIndex((item) => item.id === product.id) !== -1
-                                  ?
-                                  <div
-                                    className={`hidden absolute top-0 right-0 w-[32px] h-[32px] border-2 border-main-grey-color rounded-full text-center opacity-95 duration-500 showView hover:bg-[#b20000] hover:border-[#b20000] cursor-pointer`}
-                                    title='Remove to wishlist'
-                                    onClick={() => { handleRemoveItemToWishList(product.id) }}
-                                  >
-                                    <HeartOutlined className='text-xl text-main-grey-color mt-[5px] show-heart' />
-                                  </div>
-                                  : <></>
-                            }
-                          </div>
-                          <Link
-                            to={`${RoutePath.DetailProduct}/${product.id}`}
-                            className="text-base block leading-5 mt-2"
-                          >
-                            {product.name}
-                          </Link>
-                          <div className='flex flex-col justify-start gap-2'>
-                            {product.old_price != 0 && <span className="font-medium line-through text-category-title">{convertNumbertoMoney(product.old_price)}</span>}
-                            <div className='flex gap-3 items-center'>
-                              <span className="font-semibold">{convertNumbertoMoney(product.new_price)}</span>
-                              {product.old_price != 0 && <Tag color="#ed3324" className="font-semibold text-sm">{calculateSalePercentage(product.old_price, product.new_price)}</Tag>}
-                            </div>
-                          </div>
-                        </div>
+                        <ProductCard product={product} />
                       </SwiperSlide>)
                   }
                 </Swiper>

3316c8f5af0d14a497a959562fa5ae4a3285bca2
loc <nguyengialoc7@gmail.com>
2024-06-25 22:36:07 +0700

    search transaction by name

--- front_end/src/pages/admin/Inventory/index.tsx
+++ front_end/src/pages/admin/Inventory/index.tsx
@@ -167,7 +167,6 @@ const Inventory = () => {
             key: "name",
             dataIndex: "name",
             ...getColumnSearchProps('name'),
-            // render: (_, record) => record.name
         },
         {
             title: "Giá cũ",
--- front_end/src/pages/admin/Transaction/index.tsx
+++ front_end/src/pages/admin/Transaction/index.tsx
@@ -1,11 +1,14 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useAlertDispatch } from '@/contexts/AlertContext'
 import { convertNumbertoMoney } from '@/utils'
-import { Space, Table, TablePaginationConfig, TableProps, Typography } from 'antd'
+import { Button, Input, InputRef, Space, Table, TableColumnType, TablePaginationConfig, TableProps, Typography } from 'antd'
 import axios from 'axios'
 import { Helmet } from 'react-helmet-async'
+import { FilterDropdownProps } from 'antd/es/table/interface'
+import Highlighter from 'react-highlight-words'
+import { SearchOutlined } from '@ant-design/icons';
 
-interface ITrancsaction {
+interface ITransaction {
     id: number
     description: string
     when: string
@@ -13,10 +16,16 @@ interface ITrancsaction {
 }
 
 const Transaction = () => {
-    const [dataSource, setDataSource] = useState<ITrancsaction[]>([])
+    const [dataSource, setDataSource] = useState<ITransaction[]>([])
+    console.log({ dataSource })
     const [currentPage, setCurrentPage] = useState(1)
-    const [totalRecords, setTotalRecords] = useState()
+    const [totalRecords, setTotalRecords] = useState(0)
     const dispatchAlert = useAlertDispatch()
+
+    type DataIndex = keyof ITransaction;
+    const searchInput = useRef<InputRef>(null);
+    const [searchText, setSearchText] = useState('');
+    const [searchedColumn, setSearchedColumn] = useState('');
 
     useEffect(() => {
         const getTransactions = async (currentPage: number) => {
@@ -32,7 +41,6 @@ const Transaction = () => {
                             "sort": "DESC"
                         }
                     },)
-                console.log({ res })
                 setDataSource(res.data.data.records)
                 setTotalRecords(res.data.data.totalRecords)
                 dispatchAlert({ loading: false })
@@ -43,26 +51,124 @@ const Transaction = () => {
         if (currentPage) getTransactions(currentPage)
     }, [currentPage])
 
-    const columns: TableProps<ITrancsaction>['columns'] = [
+    const handleSearch = (
+        selectedKeys: string[],
+        confirm: FilterDropdownProps['confirm'],
+        dataIndex: DataIndex,
+    ) => {
+        confirm();
+        setSearchText(selectedKeys[0]);
+        setSearchedColumn(dataIndex);
+    };
+
+    const handleReset = (clearFilters: () => void) => {
+        clearFilters();
+        setSearchText('');
+    };
+
+    const getColumnSearchProps = (dataIndex: DataIndex): TableColumnType<ITransaction> => ({
+        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+            <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
+                <Input
+                    ref={searchInput}
+                    placeholder={`Search ${dataIndex}`}
+                    value={selectedKeys[0]}
+                    onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+                    onPressEnter={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+                    style={{ marginBottom: 8, display: 'block' }}
+                />
+                <Space>
+                    <Button
+                        type="primary"
+                        onClick={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+                        icon={<SearchOutlined />}
+                        size="small"
+                        style={{ width: 90 }}
+                    >
+                        Search
+                    </Button>
+                    <Button
+                        onClick={() => clearFilters && handleReset(clearFilters)}
+                        size="small"
+                        style={{ width: 90 }}
+                    >
+                        Reset
+                    </Button>
+                    <Button
+                        type="link"
+                        size="small"
+                        onClick={() => {
+                            confirm({ closeDropdown: false });
+                            setSearchText((selectedKeys as string[])[0]);
+                            setSearchedColumn(dataIndex);
+                        }}
+                    >
+                        Filter
+                    </Button>
+                    <Button
+                        type="link"
+                        size="small"
+                        onClick={() => {
+                            close();
+                        }}
+                    >
+                        close
+                    </Button>
+                </Space>
+            </div>
+        ),
+        filterIcon: (filtered: boolean) => (
+            <SearchOutlined style={{ color: filtered ? '#1677ff' : undefined }} />
+        ),
+        onFilter: (value, record) =>
+            (record[dataIndex] ?? '')
+                .toString()
+                .toLowerCase()
+                .includes((value as string).toLowerCase()),
+        onFilterDropdownOpenChange: (visible) => {
+            if (visible) {
+                setTimeout(() => searchInput.current?.select(), 100);
+            }
+        },
+        render: (text) =>
+            searchedColumn === dataIndex ? (
+                <Highlighter
+                    highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+                    searchWords={[searchText]}
+                    autoEscape
+                    textToHighlight={text ? text.toString() : ''}
+                    className='truncate'
+                />
+            ) : (
+                <span className='truncate'>{text}</span>
+            ),
+    });
+
+    const columns: TableProps<ITransaction>['columns'] = [
         {
             title: 'Mã giao dịch',
             key: "id",
+            dataIndex: "id",
             render: (_, record) => record.id
         },
         {
             title: 'Ngày giao dịch',
             key: "when",
+            dataIndex: "when",
             render: (_, record) => record.when
         }
         ,
         {
             title: 'Mô tả',
             key: "description",
-            render: (_, record) => record.description
+            dataIndex: "description",
+            ...getColumnSearchProps('description'),
         },
         {
             title: 'Giá trị',
-            key: "id",
+            key: "amount",
+            // dataIndex: "amount",
+            sorter: (a, b) => a.amount - b.amount,
             render: (_, record) => convertNumbertoMoney(record.amount)
         }
     ]
